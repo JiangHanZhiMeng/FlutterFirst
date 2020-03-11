d2898b5c508cfcd17112105686a1f65396b61174
JiangHanZhiMeng <wen66to88@163.com>
2020-03-11 17:40:37 +0800

    导入第三方包，随机生成英文名字

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 main(List<String> args) {
   runApp(MyApp());
@@ -8,13 +9,14 @@ main(List<String> args) {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return MaterialApp(
       title: 'Welcome',
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Welecom toFlutter'),
         ),
-        body: new Center(child: new Text('Hello World')),
+        body: new Center(child: new Text(wordPair.asPascalCase),),
       ),
     );
   }
